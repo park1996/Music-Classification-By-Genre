97533fa5b2de8cba2e47735c130d67618069251c
Bilal Nurhusien <bnurhusien@sierrawireless.com>
2020-03-08 17:02:54 -0700

    Update script to download metadat and dataset

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,67 @@
 #!/bin/bash
 
-# This script will install the required python packages and download the dataset metadata.
-# Note: Python 3.6 or greater is required.
+# This script will install the required python packages and download the dataset and metadata.
 
-pip3 install numpy seaborn matplotlib scipy python-dotenv pydot
+print_help() {
+	echo "Usage:"
+	echo "$ $0  <options>"
+	echo ""
+	echo "  options:"
+	echo "	-d - Download dataset"
+	echo "	-m - Download metadata"
+	echo "	-h - This help string"
+	echo ""
+}
 
-if [ ! -f fma_metadata.zip ]; then
-	wget https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+download_meta_data=0
+download_dataset=0
+metadata_file=fma_metadata.zip
+dataset_file=fma_small.zip
+help=false
+
+
+while getopts "hdm" options; do
+	case "${options}" in
+		d)
+			download_dataset=1
+			echo "Downloading dataset"
+			;;
+		m)
+			download_metadata=1
+			echo "Downloading metadata"
+			;;
+
+		h | *)
+			print_help
+			exit 0
+			;;
+	esac
+done
+
+
+
+if [[ $download_metadata -eq 0 && $download_dataset -eq 0 ]] ; then
+	print_help
+	exit 0
 fi
 
-unzip -o fma_metadata.zip
+# Install python packages
+#pip3 install numpy seaborn matplotlib scipy python-dotenv pydot librosa
+
+
+# Download metadata
+if [[ $download_metadata -eq 1 ]] ; then
+	echo "Removing $metadata_file"
+	rm -f "$metadata_file"
+	wget https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+	unzip -o "$metadata_file"
+fi
+
+# Download dataset
+if [[ $download_dataset -eq 1 ]] ; then
+	echo "Removing $dataset_file"
+	rm -f "$dataset_file"
+	wget https://os.unil.cloud.switch.ch/fma/fma_small.zip
+	unzip -o "$dataset_file"
+fi
+
