98a3fe1394e87b037cf3199dcf00292e8629bc01
thudinh91 <thudinhj@gmail.com>
2020-03-09 20:35:27 -0700

    add k mean cluster

--- models/k_means/k_means.py
+++ models/k_means/k_means.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+
 class k_means:
-    def __init__(self):
+    def __init__(self, number):
         ''' Constructor for this class '''
+    
+    def trainWithSkLearn(self, features, numberOfClusters):
+        kmeans = KMeans(n_clusters=numberOfClusters)
+        kmeans.fit(features)
+        predicted_clusters = kmeans.predict(features)
