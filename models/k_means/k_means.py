777b07fa7e60377a8a0223ff48dec1c6f4946b7f
thudinh91 <thudinhj@gmail.com>
2020-03-18 16:58:47 -0700

    fix bugs + add accuracy rate calc

--- models/k_means/k_means.py
+++ models/k_means/k_means.py
@@ -10,47 +10,50 @@ class k_means:
     def __init__(self):
         ''' Constructor for this class '''
     
-    def trainWithSkLearn(self, features, labels):
-        kmeans = KMeans(n_clusters=numberOfClusters)
+    def trainWithSkLearn(self, features, number_of_clusters):
+        kmeans = KMeans(n_clusters=number_of_clusters)
         kmeans.fit(features)
         predicted_clusters = kmeans.predict(features)
-
-        display_clusters(predicted_clusters, labels)
-
     
-    def train(self, features, labels, trainWithSkLearn=False):
-        number_of_clusters = len(labels)
-        centers = initialize_centers(features, number_of_clusters)
+    def train(self, features, target_labels, number_of_clusters):
+        centers, indices = initialize_centers(features, number_of_clusters)
         predicted_clusters = []
         it = 0 
         while True:
-            predicted_clusters.append(assign_clusters(features, centers[-1]))
-            new_centers = kmeans_update_centers(features, predicted_clusters[-1], centers)
-            if has_converged(centers[-1], new_centers):
+            predicted_clusters = assign_clusters(features, centers)
+            new_centers = update_centers(features, predicted_clusters, number_of_clusters, centers)
+            if is_converged(centers, new_centers):
                 break
-            centers.append(new_centers)
+            centers = new_centers
             it += 1
         return (centers, predicted_clusters, it)
 
     def initialize_centers(self, features, number_of_clusters):
-        print (type(np.random.choice(features.shape[0], number_of_clusters, replace=False)))
-        return features[np.random.choice(features.shape[0], number_of_clusters, replace=False)]
+        random_indices = np.random.choice(features.shape[0], number_of_clusters, replace=False)
+        return features[random_indices], random_indices
 
-    def update_centers(self, features, labels, centers):
-        centers = np.zeros((len(labels), features.shape[0]))
-        for iteration in range(len(labels)):
-            cluster = features[labels == iteration, :]
+    def update_centers(self, features, predicted_clusters, number_of_clusters, centers):
+        centers = np.zeros((number_of_clusters, features.shape[1]))
+        for iteration in range(number_of_clusters):
+            cluster = features[predicted_clusters == iteration, :]
             # take average
             centers[iteration,:] = np.mean(cluster, axis = 0)
         return centers
 
     def assign_clusters(self, features, centers):
         distances = cdist(features, centers)
-        return np.argmin(distances, axis=0)
+        return np.argmin(distances, axis=1)
 
     def is_converged(self, centers, new_centers):  
         return (set([tuple(center) for center in centers]) == 
         set([tuple(center) for center in new_centers]))
+    
+    def accuracy_rate(self, predicted_labels, target_labels):
+        hit = 0
+        for (it in range(len(predicted_labels))):
+            if (target_labels[it] == predicted_labels[it]):
+                hit++
+        return hit/(len(predicted_labels))
 
     def display_clusters(self, clusters, labels, title, FIG=1, figsize=(8.5, 6), marketsize=4, alpha=0.75):
         plt.figure(FIG, figsize)
