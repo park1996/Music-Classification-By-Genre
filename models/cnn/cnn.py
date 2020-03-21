df3dba8eccbb6039d15173994514f28f154c1187
tianhaot <tianhaot@sfu.ca>
2020-03-20 20:46:27 -0700

    get the code ready to work

--- models/cnn/cnn.py
+++ models/cnn/cnn.py
@@ -3,12 +3,11 @@ from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 import tensorflow as tf
 
 class cnn:
-    def __init__(self):
+    def __init__(self, input_shape):
         #Model construction code based on the following links:
         #https://engmrk.com/module-22-implementation-of-cnn-using-keras/?utm_campaign=News&utm_medium=Community&utm_source=DataCamp.com
         #https://levelup.gitconnected.com/audio-data-analysis-using-deep-learning-with-python-part-2-4a1f40d3708d
         self.MODEL = Sequential()
-        input_shape=(64, 64, 3)
         self.MODEL.add(Conv2D(32, kernel_size=(5,5), input_shape=input_shape))
         self.MODEL.add(MaxPooling2D(pool_size=(2, 2)))
         self.MODEL.add(Conv2D(64, kernel_size=(5,5), input_shape=input_shape))
--- models/cnn/cnn_classification.py
+++ models/cnn/cnn_classification.py
@@ -2,6 +2,7 @@ import os
 import ntpath
 import sys
 from cnn import cnn
+import numpy as np
 
 PACKAGE_PARENT = '../..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
@@ -11,26 +12,63 @@ from audio_preprocessor import audio_preprocessor as ap
 from feature_extractor import feature_extractor as fe
 
 if __name__ == '__main__':
+
     myAP = ap()
     myFE = fe()
-    myModel = cnn()
+    myModel = cnn((num_rows, num_columns, num_channels))
     audioPathList = myAP.list_of_all_audio_files
     trainTrackID = myFE.get_training_dataset_song_ids()
-    trainData = []
-    trainGenre = []
+    trainDataLst = []
+    trainGenreLst = []
+    trainGenreDict = {}
     print('Now loading training data...')
     i = 0
+    nextGenreID = 0
+    spectrogramLowestCol = 2147483647
+    spectrogramLowestRow = 2147483647
     for path in audioPathList:
         trackIDStr = os.path.splitext(ntpath.basename(path))[0]
         trackIDInt = int(trackIDStr)
         if os.path.exists(path) and (trackIDInt in trainTrackID):
             currentGenre = myFE.get_genre(trackIDInt)
-            print('Loading track '+trackIDStr + ' it has genre '+currentGenre)
-            trainData.append(myAP.audio_read(path))
-            trainGenre.append(currentGenre)
+            currentGenreID = -1
+            currentSpectrogram = myAP.get_mel_spectrogram(path)
+            if(len(currentSpectrogram[0]) < spectrogramLowestCol):
+                spectrogramLowestCol=len(currentSpectrogram[0])
+            if(len(currentSpectrogram) < spectrogramLowestRow):
+                spectrogramLowestRow=len(currentSpectrogram[0])
+            print('Loading track '+trackIDStr + ' with genre '+currentGenre)
+            if trainGenreDict.get(currentGenre) == None:
+                trainGenreDict[currentGenre] = nextGenreID
+                currentGenreID = nextGenreID
+                nextGenreID+=1
+            else:
+                currentGenreID = trainGenreDict[currentGenre]
+            trainDataLst.append(currentSpectrogram)
+            trainGenreLst.append(currentGenreID)
             i+= 1
             if i > 10:
                 break;
-    print('Training data loaded.')
+
+    for i in range(0, len(trainDataLst)):
+        spectrogram = trainDataLst[i]
+        if len(spectrogram[0]) <= spectrogramLowestCol and len(spectrogram) <= spectrogramLowestRow:
+            continue;
+        while len(spectrogram[0]) > spectrogramLowestCol:
+            spectrogram = np.delete(spectrogram, len(spectrogram[0])-1, 1)
+        while len(spectrogram) > spectrogramLowestRow:
+            spectrogram = np.delete(spectrogram, len(spectrogram)-1, 0)
+        trainDataLst[i] = spectrogram
+    print('Creating data model...')
+    num_rows = spectrogramLowestRow
+    num_columns = spectrogramLowestCol
+    num_channels = 1
+    myModel = cnn((num_rows, num_columns, num_channels))
+    print('Training data model...')
+    trainData = np.array(trainDataLst)
+    trainData = trainData.reshape(trainData.shape[0], num_rows, num_columns, num_channels)
+    trainGenre = np.array(trainGenreLst)
+
+
     print('Now training model...')
     myModel.train(trainData, trainGenre)
