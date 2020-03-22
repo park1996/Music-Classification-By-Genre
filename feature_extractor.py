8eb2f800a70fa435b3d426f692aef825639b1317
Bilal Nurhusien <BNurhusien@sierrawireless.com>
2020-03-22 02:24:13 -0700

    Formatting

--- feature_extractor.py
+++ feature_extractor.py
@@ -174,10 +174,10 @@ class feature_extractor:
 
         if self.GENRES in filename:
             genre_list = []
-                
+
             for chunk in  pd.read_csv(filepath, index_col=0, chunksize=CHUNK_SIZE, low_memory=False):
                 genre_list.append(chunk)
-  
+
             print ('Loaded ' + filename  + '\n')
 
             return pd.concat(genre_list,sort=False)
@@ -185,18 +185,18 @@ class feature_extractor:
 
         if self.TRACKS in filename:
             track_list = []
-                
+
             for chunk in  pd.read_csv(filepath, index_col=0, header=[0, 1], chunksize=CHUNK_SIZE, low_memory=False):
                 track_list.append(chunk)
 
             tracks = pd.concat(track_list,sort=False)
-  
+
             COLUMNS = [('track', 'tags'), ('album', 'tags'), ('artist', 'tags'),
                                ('track', 'genres'), ('track', 'genres_all')]
 
             for column in COLUMNS:
                     tracks[column] = tracks[column].map(ast.literal_eval)
- 
+
             COLUMNS = [('track', 'date_created'), ('track', 'date_recorded'),
                                ('album', 'date_created'), ('album', 'date_released'),
                                ('artist', 'date_created'), ('artist', 'active_year_begin'),
@@ -247,7 +247,7 @@ class feature_extractor:
 
     def get_artist(self, track_id):
         ''' Get artist of track '''
-        ''' track_id - unique ID of the song in dataset ''' 
+        ''' track_id - unique ID of the song in dataset '''
         return self.tracks.loc[track_id, self.ARTIST][self.NAME]
 
     def get_feature(self, track_id, feat_type, stat_type):
